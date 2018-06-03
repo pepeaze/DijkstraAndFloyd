439832e25e4a23877ec5d962927d1df9962cb1d0
pepeaze <pepeaze@gmail.com>
2018-06-03 19:28:12 -0300

    script to execute all instances

--- codes/dijkstra/run-list.sh
+++ codes/dijkstra/run-list.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-echo NY distance:
-time ./a.out -d -NY -l
-echo COL distance:
-time ./a.out -d -COL -l
-echo FLA distance:
-time ./a.out -d -FLA -l
-echo BAY distance:
-time ./a.out -d -BAY -l
-echo NY timeTravel:
-time ./a.out -t -NY -l
-echo COL timeTravel:
-time ./a.out -t -COL -l
-echo FLA timeTravel:
-time ./a.out -t -FLA -l
-echo BAY timeTravel:
-time ./a.out -t -BAY -l
+echo NY distance heap:
+time ./dijkstra -d -NY -h
+echo NY distance array:
+time ./dijkstra -d -NY -v
+echo
+echo BAY distance heap:
+time ./dijkstra -d -BAY -h
+echo BAY distance array:
+time ./dijkstra -d -BAY -v
+echo
+echo COL distance heap:
+time ./dijkstra -d -COL -h
+echo COL distance array:
+time ./dijkstra -d -COL -v
+echo
+echo FLA distance heap:
+time ./dijkstra -d -FLA -h
+echo FLA distance array:
+time ./dijkstra -d -FLA -v
+#echo COL distance:
+#time ./dijkstra -d -COL -l
+#echo FLA distance:
+#time ./dijkstra -d -FLA -l
+#echo BAY distance:
+#time ./dijkstra -d -BAY -l
+#echo NY timeTravel:
+#time ./dijkstra -t -NY -l
+#echo COL timeTravel:
+#time ./dijkstra -t -COL -l
+#echo FLA timeTravel:
+#time ./dijkstra -t -FLA -l
+#echo BAY timeTravel:
+#time ./dijkstra -t -BAY -l
