2275875a4f24141bfc5db5b4df9fe3329d230d66
pepeaze <pepeaze@gmail.com>
2018-06-04 13:57:57 -0300

    updating codes

--- codes/dijkstra/main.c
+++ codes/dijkstra/main.c
@@ -58,7 +58,7 @@ int main(int argc, char **argv){
     int path_size;
 
     t_graph **adjacent_list;
-    t_path *path;
+    t_path path[3];
 
     int **adjacent_matrix;
 
@@ -76,26 +76,43 @@ int main(int argc, char **argv){
     }
 
     graph_size = get_graph_size(f);
+    path[0].src = 0;
+    path[1].src = 0;
+    path[2].src = 0;
+    path[0].dest = (graph_size-(graph_size*0.85))-1;
+    path[1].dest = (graph_size/2)-1;
+    path[2].dest = graph_size-1;
     adjacent_list = get_adjacent_list(adjacent_list, graph_size, f);
 
     if(strcmp(data_structure_type, "-v") == 0){
-        dijkstra_results = dijkstra_array (adjacent_list, graph_size, 1);
+        for(i=0;i<3;i++){
+            dijkstra_results = dijkstra_array (adjacent_list, graph_size, path[i].src, path[i].dest);
+            printf("Distance from %d to %d vertex: %d\n", path[i].src+1, path[i].dest+1,dijkstra_results.distancia[path[i].dest]);
+        }
     }
 
     if(strcmp(data_structure_type, "-h") == 0){
-        dijkstra_results = dijkstra_heap (adjacent_list, graph_size, 1);
+        for(i=0;i<3;i++){
+            dijkstra_results = dijkstra_heap (adjacent_list, graph_size, path[i].src, path[i].dest);
+            printf("Distance from %d to %d vertex: %d\n", path[i].src+1, path[i].dest+1,dijkstra_results.distancia[path[i].dest]);
+        }
     }
 
     if(strcmp(argv[2],"-TES")==0){
         for(i=0;i<graph_size;i++){
-            printf("%d\t",dijkstra_results.distancia[i]);
+            printf("%d\t",dijkstra_results.anterior[i]);
         }
         printf("\n");
     }
 
-    else{
-        printf("Distance from 1 to %d vertex: %d\n", graph_size,dijkstra_results.distancia[graph_size-1]);
-    }
+   /*else{
+        printf("Distance from 1 to %d vertex:\n", 50);
+        for(i=0;i<50;i++){
+            printf("%d\t",dijkstra_results.distancia[i]);
+        }
+        printf("\n");
+        printf("Distance from 1 to %d vertex: %d\n", 50,dijkstra_results.distancia[50-1]);
+    }*/
 
 
 
