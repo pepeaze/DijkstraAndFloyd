8392edea9ca8dc509a678255578920530dbece25
pepeaze <pepeaze@gmail.com>
2018-06-06 08:44:11 -0300

    function read_parameters transfered to sharedLibrary

--- codes/dijkstra/main.c
+++ codes/dijkstra/main.c
@@ -14,43 +14,6 @@ void print_list(t_graph *graph){
 
 }
 
-void readParameters (int argc, char **argv){
-
-    if(argc != 4){
-        printf("Parametros incorretos!\n");
-        printf("./dijkstra < -d :distanceGraph or -t :timeTravelGraph > < -NY || -COL || -FLA || -BAY || -TES (test) instances> < -v for array or -h for heap\n");
-        exit(0);
-    }
-
-    if(strcmp(argv[1],"-d")==0){
-        if(strcmp(argv[2],"-NY")==0)
-            strcpy(graph_folder,"../../instances/distanceGraphs/USA-road-d.NY.gr");
-        else if(strcmp(argv[2],"-COL")==0)
-            strcpy(graph_folder,"../../instances/distanceGraphs/USA-road-d.COL.gr");
-        else if(strcmp(argv[2],"-FLA")==0)
-            strcpy(graph_folder,"../../instances/distanceGraphs/USA-road-d.FLA.gr");
-        else if(strcmp(argv[2],"-BAY")==0)
-            strcpy(graph_folder,"../../instances/distanceGraphs/USA-road-d.BAY.gr");
-        else if(strcmp(argv[2],"-TES")==0)
-            strcpy(graph_folder,"../../instances/teste/graph.gr");
-    }
-
-    else if(strcmp(argv[1],"-t")==0){
-        if(strcmp(argv[2],"-NY")==0)
-            strcpy(graph_folder,"../../instances/travelTimeGraphs/USA-road-t.NY.gr");
-        else if(strcmp(argv[2],"-COL")==0)
-            strcpy(graph_folder,"../../instances/travelTimeGraphs/USA-road-t.COL.gr");
-        else if(strcmp(argv[2],"-FLA")==0)
-            strcpy(graph_folder,"../../instances/travelTimeGraphs/USA-road-t.FLA.gr");
-        else if(strcmp(argv[2],"-BAY")==0)
-            strcpy(graph_folder,"../../instances/travelTimeGraphs/USA-road-t.BAY.gr");
-        else if(strcmp(argv[2],"-TES")==0)
-            strcpy(graph_folder,"../../instances/teste/graph.gr");
-    }
-
-    strcpy(data_structure_type, argv[3]);
-}
-
 int main(int argc, char **argv){
 
     readParameters(argc, argv);
@@ -75,16 +38,17 @@ int main(int argc, char **argv){
         exit(0);
     }
 
-    graph_size = get_graph_size(f);
+    graph_size = get_graph_size(f, argv);
     path[0].src = 0;
-    path[1].src = 0;
-    path[2].src = 0;
     path[0].dest = (graph_size-(graph_size*0.85))-1;
+    path[1].src = 0;
     path[1].dest = (graph_size/2)-1;
+    path[2].src = 0;
     path[2].dest = graph_size-1;
-    adjacent_list = get_adjacent_list(adjacent_list, graph_size, f);
+    adjacent_list = get_adjacent_list(adjacent_list, graph_size, f, argv);
 
     if(strcmp(data_structure_type, "-v") == 0){
+        printf("dijkstra_array\n");
         for(i=0;i<3;i++){
             dijkstra_results = dijkstra_array (adjacent_list, graph_size, path[i].src, path[i].dest);
             printf("Distance from %d to %d vertex: %d\n", path[i].src+1, path[i].dest+1,dijkstra_results.distancia[path[i].dest]);
@@ -92,6 +56,7 @@ int main(int argc, char **argv){
     }
 
     if(strcmp(data_structure_type, "-h") == 0){
+        printf("dijkstra_heap\n");
         for(i=0;i<3;i++){
             dijkstra_results = dijkstra_heap (adjacent_list, graph_size, path[i].src, path[i].dest);
             printf("Distance from %d to %d vertex: %d\n", path[i].src+1, path[i].dest+1,dijkstra_results.distancia[path[i].dest]);
